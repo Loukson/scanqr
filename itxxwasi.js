a17a5c734d3ddf4c86af2d3d7cdc12d36f2675bc
Alex tv <htvirus8@gmail.com>
2024-08-09 21:19:22 +0100

    Update itxxwasi.js

--- itxxwasi.js
+++ itxxwasi.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8000;
 let server = require('./wasiqr'),
     code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/bladeqr', server);
+app.use('/wasiqr', server);
 app.use('/code', code);
 app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
